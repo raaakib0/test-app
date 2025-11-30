45a76624cdac535c62d09f4e3b71a8677677d19f
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-01 00:44:49 +0600

    wellcome page enhance

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <h1>welcome page</h1>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+  
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+            <h1>Welcome!</h1>
+            <p>You have successfully created a Laravel Blade page.</p>
+        </div>
+    </div>
+</body>
+</html>
