86f3f8bbebe5b35a9304440d9b1749003abce1f0
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-12 18:18:55 +0600

    dinamic style and button added

--- resources/js/TaskList.vue
+++ resources/js/TaskList.vue
@@ -2,6 +2,8 @@
     <div>
         <div v-for="task in tasks" :key="task.id">
             <input type="checkbox" :checked="task.completed" @change="toggle(task)">
+            <span :style="{textDecoration:task.completed? 'line-through' : 'none'}">{{ task.title }}</span>
+        <button @click="remove(task.id)" >Delete</button>
         </div>
     </div>
 </template>
