bf83a49d3a01998afac2e4dba0d511f9f6d56243
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-12 18:09:46 +0600

    input added

--- resources/js/TaskList.vue
+++ resources/js/TaskList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="task in tasks" :key="task.id">
-            
+            <input type="checkbox" :checked="task.completed" @change="toggle(task)">
         </div>
     </div>
 </template>
