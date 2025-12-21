6179c93e1a007a09265956c085724178b3fb4d17
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-21 23:25:02 +0600

    task list formated

--- resources/js/TaskList.vue
+++ resources/js/TaskList.vue
@@ -1,36 +1,44 @@
 <template>
     <div>
         <div v-for="task in tasks" :key="task.id">
-            <input type="checkbox" :checked="task.completed" @change="toggle(task)">
-            <span :style="{textDecoration:task.completed? 'line-through' : 'none'}">{{ task.title }}</span>
-        <button @click="remove(task.id)" >Delete</button>
+            <input
+                type="checkbox"
+                :checked="task.completed"
+                @change="toggle(task)"
+            />
+            <span
+                :style="{
+                    textDecoration: task.completed ? 'line-through' : 'none',
+                }"
+                >{{ task.title }}</span
+            >
+            <button @click="remove(task.id)">Delete</button>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import {defineComponent} from 'vue';
-    import axios from  'axios';
-import { Task } from "./types";
-
+import axios from 'axios';
+import { defineComponent } from 'vue';
+import { Task } from './types';
 
 export default defineComponent({
-props:{tasks: Array as ()=> Task[] },
-emit: ['updated'],
+    props: { tasks: Array as () => Task[] },
+    emit: ['updated'],
 
-setup(props,{emit}){
-const toggle =  async (task: Task)=>{
-await axios.put(`/api/tasks/${task.id}`, {completed: !task.completed});
-emit('updated');
-};
+    setup(props, { emit }) {
+        const toggle = async (task: Task) => {
+            await axios.put(`/api/tasks/${task.id}`, {
+                completed: !task.completed,
+            });
+            emit('updated');
+        };
 
-const remove= async (id: number)=> {
-await axios.delete(`/api/tasks/${id}`);
-emit('updated');
-};
-return {toggle, remove};
-}
-
+        const remove = async (id: number) => {
+            await axios.delete(`/api/tasks/${id}`);
+            emit('updated');
+        };
+        return { toggle, remove };
+    },
 });
-
 </script>
