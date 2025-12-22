f6e86f50588ebc418cba7ecf10c05180f4f0d103
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-22 22:19:54 +0600

    app file correction

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,30 +2,28 @@
     <div>
         <h1>Task Manager</h1>
         <TaskForm @created="fetchTasks" />
-        <TaskList :tasks="tasks" @updated="fetchTasks"/>
+        <TaskList :tasks="tasks" @updated="fetchTasks" />
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent,ref, onMounted  } from "vue";
 import axios from 'axios';
+import { defineComponent, onMounted, ref } from 'vue';
 import TaskForm from './TaskForm.vue';
 import TaskList from './TaskList.vue';
-import type {Task} from './type';
+import type { Task } from './type';
 
 export default defineComponent({
-    components: {TaskForm, TaskList},
+    components: { TaskForm, TaskList },
 
-setup(){
-const tasks = ref<Task[]>([]);
-const fetchTasks = async ()=>{
-    const res = await axios.get('/api/tasks');
-    tasks.value = res.data;
-}
-onMounted(fetchTasks);
-return{tasks,fetchTasks};
-}
-
-})
-
+    setup() {
+        const tasks = ref<Task[]>([]);
+        const fetchTasks = async () => {
+            const res = await axios.get('/api/tasks');
+            tasks.value = res.data;
+        };
+        onMounted(fetchTasks);
+        return { tasks, fetchTasks };
+    },
+});
 </script>
