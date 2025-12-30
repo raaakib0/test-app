8ef0860ffb97a93eec9aaa385eb65b10b5d14ca8
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-30 22:56:47 +0600

    submit button commit

--- resources/js/TaskForm.vue
+++ resources/js/TaskForm.vue
@@ -11,12 +11,12 @@ export default defineComponent({
     emits: ['created'],
     setup(_,{emit}){
         const title = ref('');
-        const submit = async() =>{
-            if(!title.value) return;
-            await axios.post('/api/tasks', {title: title.value});
-            title.value='';
-            emit('created');
-        };
+        // const submit = async() =>{
+        //     if(!title.value) return;
+        //     await axios.post('/api/tasks', {title: title.value});
+        //     title.value='';
+        //     emit('created');
+        // };
         return {title, submit};
     },
 });
